485e484971dcf4999eb2480b433188678b00d078
shubhamtewari <shbmtewari@gmail.com>
2018-12-31 15:19:11 +0530

    added poll input dialog box, fixed bits

--- src/main/java/BoardController.java
+++ src/main/java/BoardController.java
@@ -1,5 +1,3 @@
-import core.BoardStruct;
-import core.PollInputWindowController;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
--- src/main/java/PollInputWindowController.java
+++ src/main/java/PollInputWindowController.java
@@ -1,5 +1,3 @@
-package core;
-
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
